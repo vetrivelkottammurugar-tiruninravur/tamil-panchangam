4c7f1faf5149a578d5461351bc32d151b84f16c8
Vetrivel Kottam Murugar Tiruninravur <vetrivelkottam.murugar@gmail.com>
2026-04-05 19:07:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
+// config.js
+const PANCHANG_CONFIG = {
+  apiUrl: "https://astroapi-1.divineapi.com/indian-api/v2/find-panchang",
+  apiKey: "PASTE_YOUR_API_KEY_HERE",
+  token: "PASTE_YOUR_BEARER_TOKEN_HERE",
 
+  location: {
+    lat: 13.0827,     // Chennai
+    lon: 80.2707,
+    timezone: 5.5
+  },
+
+  language: "tm"      // tm = Tamil, en = English
+};
